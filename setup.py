1b7ebd44aea9ca6a2134f95a8fcd13d10d13422f
Joon Ro <joon.ro@outlook.com>
2015-10-14 12:49:32 -0500

    +author and description

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-
 setup(
     name="color-schemes-for-ipython-qtconsole",
     version="0.0",
-    author="...",
-    author_email="...",
-    description="...",
+    author="Joon Ro"
+    author_email="joon.ro@outlook.com",
+    description="Color Schemes for Jupyter Qt Console",
     packages=["ipython_colorschemes"],
     package_data={"ipython_colorschemes": ["*.css"]},
     entry_points={"pygments.styles": [
@@ -16,3 +15,4 @@ setup(
         "tomorrow = ipython_colorschemes.tomorrow:TomorrowStyle",
         "zenburn = ipython_colorschemes.zenburn:ZenburnStyle"]}
 )
+
